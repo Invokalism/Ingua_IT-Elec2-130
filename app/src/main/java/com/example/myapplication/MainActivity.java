8417d1dc0f34e80205835a513ffc272d548e2a90
Emmanuel Jose Ingua <emmanueljoseingua@gmail.com>
2023-02-18 18:20:06 +0800

    02/18/2023  Linear,Relative,Constraint Layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,19 +12,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG,"This is a verbose log.");
-        Log.d(TAG,"This is a debug log.");
-        Log.i(TAG,"This is an info log.");
-        Log.w(TAG,"This is a warn log.");
-        Log.e(TAG,"This is an error log.");
-
         Button button = (Button) findViewById(R.id.button2);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +28,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
     }
-}
