1e95fc3c608e277dc03c31fee88566d24ede3c32
Admin <emmanueljoseingua@gmail.com>
2023-03-04 17:03:22 +0800

    inputs

--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class ThirdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
+        Button button = (Button) findViewById(R.id.button5);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ThirdActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
